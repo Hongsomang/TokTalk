46dece7966f8bce11a6b85d984ff49869b7c3f30
Hongsomang <ghdthakd12@naver.com>
2017-10-27 16:16:50 +0900

    sinup

--- TokTalk/app/src/main/java/com/example/dsm2016/toktalk/LoginActivity.java
+++ TokTalk/app/src/main/java/com/example/dsm2016/toktalk/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.dsm2016.toktalk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,5 +20,13 @@ public class LoginActivity extends AppCompatActivity {
         idfield=(EditText)findViewById(R.id.login_id_et);
         pwdfield=(EditText)findViewById(R.id.login_pwd_et);
         signup_btn=(TextView)findViewById(R.id.signup_btn);
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),SignupAcitivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
